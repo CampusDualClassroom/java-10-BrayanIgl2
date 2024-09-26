f0c0e3239d83e13f8c94f5d77cf9ceba861d62f2
Brayan Iglesias <iglesiasbrayan2@gmail.com>
2024-09-26 12:08:47 +0200

    Ej 10 OK

--- src/main/java/com/campusdual/classroom/Exercise10.java
+++ src/main/java/com/campusdual/classroom/Exercise10.java
@@ -1,5 +1,7 @@
 package com.campusdual.classroom;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Exercise10 {
@@ -10,8 +12,19 @@ public class Exercise10 {
     // COLOR_BOLA puede ser → rojo, azul, verde
     public static void main(String[] args) {
 
-    }
+        //Contador de azules
+        int contadorAzul = 0;
 
+        //Obtiene colores en bucle hasta obtener 2 azules
+        do {
+            String pelota = getBall();
+            if (pelota.equals("azul")) contadorAzul++;
+            System.out.println("La bola es de color: "+pelota);
+        }while (contadorAzul != 2);
+
+
+
+    }
     //TODO ↓
     // Un método que devuelva una cadena de texto con el color de una pelota (en minúscula). Dicho color se escogerá de
     // manera aleatoria. Se empleará el método randomWithRange(int min, int max) para escoger un número aleatorio, que
@@ -20,10 +33,22 @@ public class Exercise10 {
     // 2 → azul
     // 3 → verde
     public static String getBall() {
+
+        int n = randomWithRange(1, 4);
+
+        String pelota = null;
+        if (n == 1) {
+            return "rojo";
+        } else if (n == 2) {
+            return "azul";
+        } else if (n == 3) {
+            return "verde";
+        }
         return null;
     }
 
     public static int randomWithRange(int min, int max) {
-        return ThreadLocalRandom.current().nextInt(min,max);
+
+        return ThreadLocalRandom.current().nextInt(min, max);
     }
 }
